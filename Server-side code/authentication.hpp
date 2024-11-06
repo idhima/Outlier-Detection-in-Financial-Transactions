1ab8c17f31f3a680aef13c27d7965dea5d380298
Ridhima <162116052+idhima@users.noreply.github.com>
2024-11-07 00:35:16 +0530

    Create authentication.hpp

---
+++ Server-side code/authentication.hpp
@@ -0,0 +1,8 @@
+#ifndef AUTHENTICATION_HPP
+#define AUTHENTICATION_HPP
+
+#include <string>
+
+bool authenticateUser(const std::string& username, const std::string& password);
+
+#endif // AUTHENTICATION_HPP
