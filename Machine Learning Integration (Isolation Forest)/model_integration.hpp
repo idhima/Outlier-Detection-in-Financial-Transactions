0682c157739d48cecbee7fee1310cfe8acf5e0b7
Ridhima <162116052+idhima@users.noreply.github.com>
2024-11-07 00:38:06 +0530

    Create model_integration.hpp

---
+++ Machine Learning Integration (Isolation Forest)/model_integration.hpp
@@ -0,0 +1,9 @@
+#ifndef MODEL_INTEGRATION_HPP
+#define MODEL_INTEGRATION_HPP
+
+#include <vector>
+#include <string>
+
+std::vector<bool> detectAnomaliesUsingML(const std::vector<double>& transactions);
+
+#endif // MODEL_INTEGRATION_HPP
