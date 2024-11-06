a7b12ad7f0b13b4a66ee4a776497ade169249a57
Ridhima <162116052+idhima@users.noreply.github.com>
2024-11-07 00:36:23 +0530

    Create report_generator.hpp

---
+++ Server-side code/report_generator.hpp
@@ -0,0 +1,9 @@
+#ifndef REPORT_GENERATOR_HPP
+#define REPORT_GENERATOR_HPP
+
+#include <vector>
+#include <string>
+
+void generateReport(const std::vector<double>& transactions, const std::string& reportPath);
+
+#endif // REPORT_GENERATOR_HPP
